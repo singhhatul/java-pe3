c7d67ed1b328dbc49a0aa6aeda6017cc6310b7b9
atul.kumar1 <atul.kumar1@cgi.com>
2019-07-23 10:07:14 +0530

    chessboard correction

--- src/main/java/com/stackroute/pe3/ChessBoard.java
+++ src/main/java/com/stackroute/pe3/ChessBoard.java
@@ -1,12 +1,23 @@
-//package com.stackroute.pe3;
-//
-//public class ChessBoard {
-//    public String[][] getChessBoard(int row,int col){
-//
-//        String[8][8] chesboard;
-//        if(row<0 && col <0)
-//            return null;
-//        if()
-//
-//    }
-//}
+package com.stackroute.pe3;
+
+public class ChessBoard {
+    public String[][] getChessBoard(int row, int col) {
+        String resultArray[][] =null;
+        if (row < 0 || col < 0) {
+            return resultArray;
+        } else {
+            resultArray = new String[row][col];
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    if (((i + j) % 2) == 0) {
+                        resultArray[i][j] = "WW|";
+                    } else {
+                        resultArray[i][j] = "BB|";
+                    }
+                }
+            }
+            return resultArray;
+        }
+    }
+}
+
